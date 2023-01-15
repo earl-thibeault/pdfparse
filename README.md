490ff48e85c3320878691eec693bcc5801d1164c
earl-thibeault <ect8988@gmail.com>
2023-01-15 05:37:16 -0500

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   README.md
    	new file:   pdfparse.py

---
+++ README.md
@@ -0,0 +1,8 @@
+README.md
+pdfparse.py 
+by Earl Thibeault
+python 3.9
+requires pdftotext from The Poppler Developers usally comes with the xpdf pkg.
+So far only 2 functions pdf2txt a bare bones and dumbed down pdftotext wrapper and the simptbl a 
+more complex function that takes the resulting txt file and turns it to a csv for now just import 
+from a copy in folder may expand later.
---
+++ pdfparse.py
@@ -0,0 +1,49 @@
+import re
+import csv
+import subprocess
+##pdf is name of the pdf out is outfile name opts as of now ive only done 2
+def pdf2txt(pdf,txt,**opts):
+    cmd=['pdftotext']
+    if not opts.get('layoput')==False:
+        cmd.append('-layout')
+    if not opts.get('nopgbrk')==False:
+        cmd.append('-nopgbrk')
+  ##there are a bunch more options nopgbrk and layout are the 2 i use and they are default true
+    cmd.append(pdf)
+    cmd.append(txt)
+    subprocess.call(cmd)
+    return txt
+def simptbl(txt, nocopy, out, fieldlst):
+    lnlst=[]
+    with open(txt, 'r')as tf:
+        for ln in tf.readlines():
+            rlst=[]
+            ln=str(ln)
+            if not ln in nocopy:    
+                rlst=[]
+                row=ln.split('  ')
+                for wrd in row:
+                    if not wrd=='':
+                      rlst.append(wrd)
+                if len(rlst)>0:
+                    lnlst.append(rlst)
+    dictlst=[]            
+    for r in lnlst:
+        if len(r)==len(fieldlst):
+            rdict=dict.fromkeys(fieldlst,value='?')
+            for i, field in enumerate(fieldlst):
+                n=i-1
+                rdict.update({field:r[n]})
+            dictlst.append(rdict)
+        else:
+            with open('.err.log', 'a') as log:
+                errlst=['ERROR WHITH LINE \n',' '.join(r)]
+                err=''.join(errlst)
+                log.write(err)
+    with open(out, 'w')as ouf:        
+        wo=csv.DictWriter(ouf, fieldlst)
+        wo.writeheader()
+        wo.writerows(dictlst)
+                               
+                
+                
